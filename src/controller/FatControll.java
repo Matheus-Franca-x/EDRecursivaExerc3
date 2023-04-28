76d9baf9b2f7564e9d472e009d4a2d95fc1abb9f
Matheus <matheussss6613@gmail.com>
2023-04-28 04:15:34 -0300

    Fim

--- src/controller/FatControll.java
+++ src/controller/FatControll.java
@@ -10,7 +10,7 @@ public class FatControll
 	public int fat(int valor) //funcao do fat
 	{
 		return valor == 0 ? 1 : valor * fat(valor - 1);		
-		//retorna 1 para caso o valor seja = 0, isso fara com que a 
-		//proxima recursiva seja multiplicado por 1
+		//retorna 1 para caso o valor seja = 0, isso fara com que a proxima recursiva seja multiplicado por 1
+		//multiplicando com os valores das recusivas anteriores.
 	}
 }
